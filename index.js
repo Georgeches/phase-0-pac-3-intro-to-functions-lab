6135810b9754130c0ebcf54db84d9e2b4d6bdb3f
Georgeches <georgechesire83@gmail.com>
2023-03-15 22:50:53 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,34 @@
+ function shout(string){
+    return string.toUpperCase()
+ }
+
+
+ function whisper(string){
+    return string.toLowerCase()
+ }
+
+ function logShout(string){
+    console.log(string.toUpperCase())
+ }
+
+ function logWhisper(string){
+    console.log(string.toLowerCase())
+ }
+
+function sayHiToHeadphonedRoommate(string){
+    if(string=="hello"){
+        return "I can\'t hear you!"
+    }
+ }
+
+function sayHiToHeadphonedRoommate(string){
+    if(string=="HELLO"){
+        return "YES INDEED!"
+    }
+}
+
+function sayHiToHeadphonedRoommate(string){
+    if(string=="Let's have dinner together!"){
+        return "I would love to!"
+    }
+}
